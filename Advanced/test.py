4c778df9c0b0eb33b9c66f583ca6da7b47307d9a
gdatskov <103522803+gdatskov@users.noreply.github.com>
2022-06-08 21:48:50 +0300

    Perlin tests

--- Advanced/test.py
+++ Advanced/test.py
@@ -1,2 +1,7 @@
-x = float(input())
-print(int(x))
+faded_x = np.arange(0, y_number, 1/gradient_points)
+faded_y = []
+for i in random_int_array:
+    faded_y.append(i)
+    for j in range(gradient_points):
+        faded_y.append(lerp(perlin_fade_func(1/j), i, i+1)
+print(faded_y)
