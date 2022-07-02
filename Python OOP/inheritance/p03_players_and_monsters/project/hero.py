cde9cf6477e3de6bd63ea80d94bd1a76ce66920a
gdatskov <103522803+gdatskov@users.noreply.github.com>
2022-07-03 00:42:06 +0300

    [answer to why it works] Inheritance - Exercise - Program 03. Players and monsters

--- Python OOP/inheritance/p03_players_and_monsters/project/hero.py
+++ Python OOP/inheritance/p03_players_and_monsters/project/hero.py
@@ -2,9 +2,11 @@ class Hero:
     def __init__(self, username, level):
         self.username = username
         self.level = level
+        # self.class_name = self.__class__.__name__
 
-    def get_class_name(self):
-        return self.__class__.__name__
+    # def get_class_name(self):
+    #     return self.__class__.__name__
 
     def __str__(self):
-        return f"{self.username} of type {self.get_class_name()} has level {self.level}"
+        # return f"{self.username} of type {self.get_class_name()} has level {self.level}"
+        return f"{self.username} of type {self.__class__.__name__} has level {self.level}"
