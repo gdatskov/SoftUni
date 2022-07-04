d363242538408421323021f34719983e9bcc8c66
gdatskov <103522803+gdatskov@users.noreply.github.com>
2022-07-04 13:25:26 +0300

    Python OOP exercise - Problem 05: Shop

---
+++ Python OOP/inheritance/p05_shop/project/product_repository.py
@@ -0,0 +1,24 @@
+from project.product import Product
+
+
+class ProductRepository:
+
+    products = list()
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name):
+        for x in self.products:
+            if x.name == product_name:
+                return x
+
+    def remove(self, product_name):
+        # self.products.remove(x for x in self.products if x.name == product_name)
+        for x in self.products:
+            if x.name == product_name:
+                self.products.remove(x)
+
+    def __repr__(self):
+        data = [f"{product.name}: {product.quantity}" for product in self.products]
+        return "\n".join(data)
