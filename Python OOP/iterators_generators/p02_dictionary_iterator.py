65ae01811da5b6a3e94501d889bfad959a65386d
gdatskov <103522803+gdatskov@users.noreply.github.com>
2022-07-23 19:51:53 +0300

    Iterators and Generators - Exercise - Program 02. Dictionary iterator

---
+++ Python OOP/iterators_generators/p02_dictionary_iterator.py
@@ -0,0 +1,20 @@
+class dictionary_iter:
+    def __init__(self, dic):
+        self.dic = dic
+        self.keys = list(dic.items())
+        self.current_idx = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current_idx == len(self.dic):
+            raise StopIteration
+        to_return = self.keys[self.current_idx]
+        self.current_idx += 1
+        return to_return
+
+
+result = dictionary_iter({1: "1", 2: "2"})
+for x in result:
+    print(x)
