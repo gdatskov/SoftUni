69079437589d8ca9bb0f722f548382b7811d0d09
gdatskov <103522803+gdatskov@users.noreply.github.com>
2022-07-23 20:59:24 +0300

    Iterators and Generators - Exercise - Program 05. Take halves generator

---
+++ Python OOP/iterators_generators/p05_take_halves.py
@@ -0,0 +1,29 @@
+def solution():
+    def integers():
+        result = 0
+        # lis = []
+        while True:
+            result += 1
+            # print(result)
+            yield result
+
+    def halves():
+        for i in integers():
+            yield i/2
+
+    def take(n, seq):
+        result = []
+        for _ in range(n):
+            result.append(next(seq))
+        return result
+
+    return (take, halves, integers)
+
+# Sample input
+
+take = solution()[0]
+halves = solution()[1]
+print(take(5, halves()))
+
+# Expected output
+# [0.5, 1.0, 1.5, 2.0, 2.5]
