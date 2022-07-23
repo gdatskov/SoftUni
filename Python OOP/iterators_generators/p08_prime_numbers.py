80cf078a3c06d9d8e70b53c0208921c51e12f221
gdatskov <103522803+gdatskov@users.noreply.github.com>
2022-07-23 22:17:44 +0300

    Iterators and Generators - Exercise - Program 08. Prime Numbers

---
+++ Python OOP/iterators_generators/p08_prime_numbers.py
@@ -0,0 +1,21 @@
+def is_prime(n: int) -> bool:
+    """Primality test using 6k+-1 optimization."""
+    if n <= 3:
+        return n > 1
+    if not n % 2 or not n % 3:
+        return False
+    i = 5
+    stop = int(n ** 0.5)
+    while i <= stop:
+        if not n % i or not n % (i + 2):
+            return False
+        i += 6
+    return True
+
+
+def get_primes(lis):
+    for num in lis:
+        if is_prime(num):
+            yield num
+
+print(list(get_primes([2, 4, 3, 5, 6, 9, 1, 0])))
