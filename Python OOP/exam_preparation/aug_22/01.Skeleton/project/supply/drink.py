313ee98e69a7f6f730b1fd3de29b904e8d3139eb
gdatskov <103522803+gdatskov@users.noreply.github.com>
2022-08-07 00:39:24 +0300

    Exam prep exercise

--- Python OOP/exam_preparation/aug_22/01.Skeleton/project/supply/drink.py
+++
@@ -1,8 +0,0 @@
-from project.supply.supply import Supply
-
-
-class Drink(Supply):
-    INITIAL_ENERGY = 15
-
-    def __init__(self, name, energy=INITIAL_ENERGY):
-        super().__init__(name, energy)
